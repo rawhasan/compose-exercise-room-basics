be147239d080c4b866d9f32f4952e38f73c3bbcd
rawhasan <rawhasanweb@gmail.com>
2021-08-11 22:03:37 +0600

    entity, dao, database

--- app/src/main/java/com/example/roombasics/MainActivity.kt
+++ app/src/main/java/com/example/roombasics/MainActivity.kt
@@ -14,25 +14,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RoomBasicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            WordBookApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun WordBookApp() {
+    Text(text = "Hello!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    RoomBasicsTheme {
-        Greeting("Android")
-    }
+    WordBookApp()
 }
