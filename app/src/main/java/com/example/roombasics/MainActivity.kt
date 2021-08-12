52bb7bb0ce85d65a1edbb7ce8016b40e73302107
rawhasan <rawhasanweb@gmail.com>
2021-08-12 12:39:32 +0600

    repository, view model, UI

--- app/src/main/java/com/example/roombasics/MainActivity.kt
+++ app/src/main/java/com/example/roombasics/MainActivity.kt
@@ -3,12 +3,24 @@ package com.example.roombasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.roombasics.ui.theme.RoomBasicsTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +33,69 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WordBookApp() {
-    Text(text = "Hello!")
+    var newWord by remember { mutableStateOf("") }
+    val numbers = (1..100).toList()
+
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+    ) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            TextField(
+                value = newWord,
+                onValueChange = { newWord = it },
+                label = { Text("New Word") },
+                modifier = Modifier.weight(1f)
+            )
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .height(56.dp)
+                    .padding(start = 8.dp)
+            ) {
+                Text("Add")
+            }
+        }
+
+        LazyColumn(
+            modifier = Modifier
+                .weight(1f)
+                .padding(vertical = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            items(numbers) { number ->
+                WordItemLayout(number.toString())
+            }
+        }
+
+        Button(onClick = { /*TODO*/ }) {
+            Text(
+                "Clear Words",
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+@Composable
+fun WordItemLayout(word: String) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colors.primaryVariant)
+            .padding(vertical = 20.dp, horizontal = 24.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(painter = painterResource(R.drawable.ic_star), contentDescription = null)
+        Text(
+            text = word,
+            color = Color.White,
+            fontSize = 20.sp,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
